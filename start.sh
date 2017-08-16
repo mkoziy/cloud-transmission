2dfd74a96c3c2b2c90efa0e451f6f8e1ff2afc51
Michael Koziy <michael.koziy@gmail.com>
2017-08-16 22:15:27 +0200

    Fixed ports firewall rules

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-ufw allow ssh
-ufw allow 9091/TCP
-ufw allow 80/TCP
-ufw deny out 51413/TCP
-ufw deny out 51413/UDP
-ufw enable
+ufw deny out 51413/tcp
+ufw deny out 51413/udp
+ufw --force enable
 
 docker-compose up -d
