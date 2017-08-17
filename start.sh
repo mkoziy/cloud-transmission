03132c688bffe8eeeb5736215ae01f69c921d82b
Michael Koziy <michael.koziy@gmail.com>
2017-08-17 21:42:04 +0200

    Changed a folder structure

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-ufw deny out 51413/tcp
-ufw deny out 51413/udp
+ufw default deny incoming
+ufw default deny outgoing
 ufw allow 9091
 ufw allow ssh
+ufw allow out 51413/tcp
+ufw allow out 51413/udp
 ufw --force enable
 
 docker-compose up -d
+
+#for i in *mkv; do ffmpeg -i $i -vcodec copy -acodec copy "${i%.mkv}.mp4" > /tmp/ffmpeg.log && rm $i; done
