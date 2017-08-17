a6ebff6489c85a99da2057baf7f1bdd3c50b6d7f
Michael Koziy <michael.koziy@gmail.com>
2017-08-17 22:43:27 +0200

    Added nginx settings

---
+++ nginx/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
