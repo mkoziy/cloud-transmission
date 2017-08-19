97f05af2d995488dac15cb46b34c07725f513d07
Michael Koziy <michael.koziy@gmail.com>
2017-08-19 12:11:42 +0200

    Fixed a firewall rules

--- start.sh
+++ start.sh
@@ -3,12 +3,9 @@
 ufw default deny incoming
 ufw default deny outgoing
 ufw allow 9091
-ufw allow 443
 ufw allow ssh
-ufw allow out 51413/tcp
-ufw allow out 51413/udp
-ufw --force enable
+ufw allow in 51413
 
 docker-compose up -d
 
-#for i in *mkv; do ffmpeg -i $i -vcodec copy -acodec copy "${i%.mkv}.mp4" > /tmp/ffmpeg.log && rm $i; done
+ufw --force enable
